1d20fafe0824ab0bb141721fc02be9abfc6742ac
Arbeliwus <75787992+Arbeliwus@users.noreply.github.com>
2023-01-31 21:23:26 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function readfile(path) {//Ajax
     const xhr = new XMLHttpRequest();
-    xhr.open('GET',"https://arbeliwus.github.io/web_test/"+path, true);//連線設定
+    xhr.open('GET',"https://127.0.0.1"+path, true);//連線設定
     xhr.onload=function(){//偵測連線狀態結束
     console.log(this.responseText);
     console.log(this.responseText[0]);
